84630489bac6d8d5bdbf6f3455f49de25f662592
WesCarvalho <weslley_lopes@hotmail.com>
2021-06-02 13:10:11 -0300

    variaveis-tipos-argumentos

---
+++ argumentos.js
@@ -0,0 +1,4 @@
+let nome = process.argv[2];
+let idade = process.argv[3];
+
+console.log("O nome é:", nome, "e a iadde é:", idade);
---
+++ variaveis-constantes.js
@@ -0,0 +1,10 @@
+ //var é uma variavel global
+var cor = "branca";
+
+// let é uma variavel que fica disponivel somente dentro do escopo onde foi atribuida.
+let nome = "Weslley"; 
+
+// const é uma variavel que não muda de valor
+const empresa = "ABC Ltda";
+
+console.log(cor, nome, empresa);
